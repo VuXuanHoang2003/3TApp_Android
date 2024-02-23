74da113ad008b33e4fcd634b053058d6e34d5da3
DangHuan2k3 <huan.bk.0412@gmail.com>
2024-02-23 16:15:13 +0700

    fix ui, statistic each type of products

--- lib/data/repositories/statistic_repo/statistic_repo_impl.dart
+++ lib/data/repositories/statistic_repo/statistic_repo_impl.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:three_tapp_app/data/repositories/statistic_repo/statistic_repo.dart';
 import 'package:three_tapp_app/model/statistic.dart';
@@ -6,11 +8,11 @@ class StatisticRepoImpl extends StatisticRepo {
   static Statistic statistic = Statistic(
       numberOfPosts: 0,
       numberOfSuccessfulTrade: 0,
-      doanh_thu_giay: 0,
-      doanh_thu_khac: 0,
-      doanh_thu_kim_loai: 0,
-      doanh_thu_nhua: 0,
-      doanh_thu_thuy_tinh: 0);
+      glassRevenue: 0,
+      metalRevenue: 0,
+      otherRevenue: 0,
+      paperRevenue: 0,
+      plasticRevenue: 0);
   @override
   Future<Statistic> getStatistic(String? email) async {
     print(email);
@@ -40,16 +42,113 @@ class StatisticRepoImpl extends StatisticRepo {
           .collection("ORDERS")
           .where("status", isEqualTo: "DONE")
           .where("seller_email", isEqualTo: email)
+          .where("type", isEqualTo: "kim_loai")
           .get()
           .then((querySnapshot) {
+        statistic.metalRevenue = 0;
         querySnapshot.docs.forEach((doc) {
           // Thực hiện các thao tác với từng phần tử doc ở đây
           var data = doc.data();
           // Ví dụ: Lấy giá trị của trường "fieldName"
-          var fieldValue = data['fieldName'];
+          print(data['product_mass']);
+          print(data['product_price']);
+          var product_mass = double.parse(data['product_mass'].toString());
+          var product_price = double.parse(data['product_price'].toString());
+
+          statistic.metalRevenue += (product_mass * product_price);
           // Tiếp tục xử lý dữ liệu ở đây
         });
       });
+
+      await FirebaseFirestore.instance
+          .collection("ORDERS")
+          .where("status", isEqualTo: "DONE")
+          .where("seller_email", isEqualTo: email)
+          .where("type", isEqualTo: "nhua")
+          .get()
+          .then((querySnapshot) {
+        statistic.plasticRevenue = 0;
+        querySnapshot.docs.forEach((doc) {
+          // Thực hiện các thao tác với từng phần tử doc ở đây
+          var data = doc.data();
+          // Ví dụ: Lấy giá trị của trường "fieldName"
+          print(data['product_mass']);
+          print(data['product_price']);
+          var product_mass = double.parse(data['product_mass'].toString());
+          var product_price = double.parse(data['product_price'].toString());
+
+          statistic.plasticRevenue += (product_mass * product_price);
+          // Tiếp tục xử lý dữ liệu ở đây
+        });
+      });
+
+      await FirebaseFirestore.instance
+          .collection("ORDERS")
+          .where("status", isEqualTo: "DONE")
+          .where("seller_email", isEqualTo: email)
+          .where("type", isEqualTo: "giay")
+          .get()
+          .then((querySnapshot) {
+        statistic.paperRevenue = 0;
+        querySnapshot.docs.forEach((doc) {
+          // Thực hiện các thao tác với từng phần tử doc ở đây
+          var data = doc.data();
+          // Ví dụ: Lấy giá trị của trường "fieldName"
+          print(data['product_mass']);
+          print(data['product_price']);
+          var product_mass = double.parse(data['product_mass'].toString());
+          var product_price = double.parse(data['product_price'].toString());
+
+          statistic.paperRevenue += (product_mass * product_price);
+          // Tiếp tục xử lý dữ liệu ở đây
+        });
+      });
+
+      await FirebaseFirestore.instance
+          .collection("ORDERS")
+          .where("status", isEqualTo: "DONE")
+          .where("seller_email", isEqualTo: email)
+          .where("type", isEqualTo: "thuy_tinh")
+          .get()
+          .then((querySnapshot) {
+        statistic.glassRevenue = 0;
+        querySnapshot.docs.forEach((doc) {
+          // Thực hiện các thao tác với từng phần tử doc ở đây
+          var data = doc.data();
+          // Ví dụ: Lấy giá trị của trường "fieldName"
+          print(data['product_mass']);
+          print(data['product_price']);
+          var product_mass = double.parse(data['product_mass'].toString());
+          var product_price = double.parse(data['product_price'].toString());
+
+          statistic.glassRevenue += (product_mass * product_price);
+          // Tiếp tục xử lý dữ liệu ở đây
+        });
+      });
+
+      await FirebaseFirestore.instance
+          .collection("ORDERS")
+          .where("status", isEqualTo: "DONE")
+          .where("seller_email", isEqualTo: email)
+          .where("type", isEqualTo: "khac")
+          .get()
+          .then((querySnapshot) {
+        statistic.otherRevenue = 0;
+        querySnapshot.docs.forEach((doc) {
+          // Thực hiện các thao tác với từng phần tử doc ở đây
+          var data = doc.data();
+          // Ví dụ: Lấy giá trị của trường "fieldName"
+          print(data['product_mass']);
+          print(data['product_price']);
+          var product_mass = double.parse(data['product_mass'].toString());
+          var product_price = double.parse(data['product_price'].toString());
+
+          statistic.otherRevenue += (product_mass * product_price);
+          // Tiếp tục xử lý dữ liệu ở đây
+        });
+      });
+
+      statistic.valuateRevenues();
     } catch (error) {
       print("error:${error.toString()}");
     }
--- lib/model/statistic.dart
+++ lib/model/statistic.dart
@@ -2,39 +2,39 @@ class Statistic {
   int numberOfPosts = 0;
   int numberOfSuccessfulTrade = 0;
 
-  double doanh_thu_kim_loai = 0;
-  double doanh_thu_giay = 0;
-  double doanh_thu_nhua = 0;
-  double doanh_thu_thuy_tinh = 0;
-  double doanh_thu_khac = 0;
+  double metalRevenue = 0;
+  double paperRevenue = 0;
+  double plasticRevenue = 0;
+  double glassRevenue = 0;
+  double otherRevenue = 0;
 
-  double doanh_thu = 0;
+  double sumOfRevenues = 0;
 
   Statistic(
       {required this.numberOfPosts,
       required this.numberOfSuccessfulTrade,
-      required this.doanh_thu_kim_loai,
-      required this.doanh_thu_giay,
-      required this.doanh_thu_nhua,
-      required this.doanh_thu_thuy_tinh,
-      required this.doanh_thu_khac});
+      required this.metalRevenue,
+      required this.paperRevenue,
+      required this.plasticRevenue,
+      required this.glassRevenue,
+      required this.otherRevenue});
 
-  valuateDoanhThu() {
-    doanh_thu = doanh_thu_giay +
-        doanh_thu_khac +
-        doanh_thu_kim_loai +
-        doanh_thu_nhua +
-        doanh_thu_kim_loai;
+  valuateRevenues() {
+    sumOfRevenues = paperRevenue +
+        otherRevenue +
+        metalRevenue +
+        plasticRevenue +
+        plasticRevenue;
   }
 
   Statistic.fromJson(Map<String, dynamic> json) {
     numberOfPosts = json['numberOfPosts'];
     numberOfSuccessfulTrade = json['numberOfSuccessfulTrade'];
-    doanh_thu_kim_loai = json['doanh_thu_kim_loai'];
-    doanh_thu_giay = json['doanh_thu_giay'];
-    doanh_thu_nhua = json['doanh_thu_nhua'];
-    doanh_thu_thuy_tinh = json['doanh_thu_thuy_tinh'];
-    doanh_thu_khac = json['doanh_thu_khac'];
-    doanh_thu = json['doanh_thu'];
+    metalRevenue = json['metalRevenue'];
+    paperRevenue = json['paperRevenue'];
+    plasticRevenue = json['plasticRevenue'];
+    glassRevenue = json['glassRevenue'];
+    otherRevenue = json['otherRevenue'];
+    sumOfRevenues = json['sumOfRevenues'];
   }
 }
